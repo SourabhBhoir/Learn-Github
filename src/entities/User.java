9884d533d88a9bb30e1f5f6e7a8ad523d2de49a1
Admin <deepaksonavale29@gmail.com>
2026-08-21 16:42:41 +0530

    update

--- src/entities/User.java
+++ src/entities/User.java
@@ -3,4 +3,8 @@ package entities;
 public class User {
     private Integer id;
     private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String status;
 }
